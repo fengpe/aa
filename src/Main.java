b0c5d04d10819ed45261232df926cd6ce5456ef8
fengpe <7400676559@qq.com>
2022-07-31 11:09:16 +0800

    delete third line commit5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("你好，世界");
         System.out.println("fourth line");
         System.out.println("add master line");
-        System.out.println("fifth line");
+        System.out.println("sixth line");
     }
 }
