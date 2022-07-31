5f7c9b52ad3b886e6339b96e0f391f916e8aaf07
fengpe <7400676559@qq.com>
2022-07-31 11:03:16 +0800

    delete third line commit2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("你好，世界");
+        System.out.println("fourth line");
         System.out.println("add master line");
     }
 }
